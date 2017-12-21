09be1a5c3b592449aca0e80dd9eeaef4f9d076e6
sheikhqumrunnessa <sheikhqumrunnessa@gmail.com>
2017-12-21 13:02:46 +0600

    new index.php file create

---
+++ syntax.php
@@ -0,0 +1,121 @@
+<?php
+   //echo "Helo World";
+//    echo "<html>";
+//      echo "<head>";
+//          echo "<title>";
+//            echo "HOME";
+//          echo "</title>";
+//      echo "</head>";
+//      echo "<body>";
+//          echo "<h1>This is a heading</h1>";
+//          echo "<p>This is a paragraph</p>";
+//      echo "<body>";
+//    echo "<html>";
+ ?>
+<!--<html>-->
+<!--  <head>-->
+<!--    <title> --><?php //echo "Home";?><!--</php></title>-->
+<!--  </head>-->
+<!--  <body>-->
+<!--     <h1>--><?php //echo "This is a heading."; ?><!--</h1>-->
+<!--     <p>--><?php //echo "This is a paragraph."; ?><!--</p>-->
+<!--  <input type="text" value="--><?php //echo "Hello World";?><!--"/>-->
+<!--  </body>-->
+<!--</html>-->
+<?php
+//$ standerd rules for variable creating
+//    start with $ sign
+//A-Z,a-z,0-9,_
+//No number in first
+
+
+
+// In creating variable standerd rules
+
+//small letter
+//meaningfull
+//readable
+
+//$Name;
+//$NAME;
+//$name;
+//$NaMe;
+//$_9name;
+//$name9;
+//
+//$number=100;
+//$firstName="Rakib";
+//$lastName="Hasan";
+// echo $firstName.' '.$lastName;
+//variable
+//$name="Rakib";
+//ctrl+spacebar
+
+
+
+    /*Arithmetic Operator
+    Binary(+,-,*,/,%)
+     Unary(++,--,(-))
+    Assignment Operator
+     =, +=, -=, *=, /=, %=
+    Conditional Operator
+    >, >=, <, <=, !=, ===, !==
+    Logical Operator
+    &&, ||, !
+*/
+//$x=10;
+//$y=20;
+//echo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//statement
+/*
+ * Single line statement
+ * Conditional statement
+     * if
+     * if else
+     *if else if
+     * switch
+ * Repeated statement
+     *for
+     * while
+     * do while
+     * foreach
+ */
+//$x=10;
+//$y=20;
+//$z =$x+$y;
+//if($x>$y){
+//    $z=$x+$y;
+//    echo $z;
+//}
+
+?>
+
+<?php
+
+function demo(){
+    echo "Hello World";
+}
+demo();
+?>
