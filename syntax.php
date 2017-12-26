5010381589966cf9e73931f41fecdab9335307df
sheikhqumrunnessa <sheikhqumrunnessa@gmail.com>
2017-12-26 08:45:16 +0600

    new syntax.php file create

--- syntax.php
+++ syntax.php
@@ -1,5 +1,5 @@
 <?php
-   //echo "Helo World";
+//echo "Helo World";
 //    echo "<html>";
 //      echo "<head>";
 //          echo "<title>";
@@ -11,240 +11,270 @@
 //          echo "<p>This is a paragraph</p>";
 //      echo "<body>";
 //    echo "<html>";
- ?>
-<!--<html>-->
-<!--  <head>-->
-<!--    <title> --><?php //echo "Home";?><!--</php></title>-->
-<!--  </head>-->
-<!--  <body>-->
-<!--     <h1>--><?php //echo "This is a heading."; ?><!--</h1>-->
-<!--     <p>--><?php //echo "This is a paragraph."; ?><!--</p>-->
-<!--  <input type="text" value="--><?php //echo "Hello World";?><!--"/>-->
-<!--  </body>-->
-<!--</html>-->
+?>
+    <!--<html>-->
+    <!--  <head>-->
+    <!--    <title> --><?php //echo "Home";?><!--</php></title>-->
+    <!--  </head>-->
+    <!--  <body>-->
+    <!--     <h1>--><?php //echo "This is a heading."; ?><!--</h1>-->
+    <!--     <p>--><?php //echo "This is a paragraph."; ?><!--</p>-->
+    <!--  <input type="text" value="--><?php //echo "Hello World";?><!--"/>-->
+    <!--  </body>-->
+    <!--</html>-->
 <?php
-//$ standerd rules for variable creating
-//    start with $ sign
-//A-Z,a-z,0-9,_
-//No number in first
+            //$ standerd rules for variable creating
+            //    start with $ sign
+            //A-Z,a-z,0-9,_
+            //No number in first
 
 
 
-// In creating variable standerd rules
+            // In creating variable standerd rules
 
-//small letter
-//meaningfull
-//readable
+            //small letter
+            //meaningfull
+            //readable
 
-//$Name;
-//$NAME;
-//$name;
-//$NaMe;
-//$_9name;
-//$name9;
-//
-//$number=100;
-//$firstName="Rakib";
-//$lastName="Hasan";
-// echo $firstName.' '.$lastName;
-//variable
-//$name="Rakib";
-//ctrl+spacebar
+            //$Name;
+            //$NAME;
+            //$name;
+            //$NaMe;
+            //$_9name;
+            //$name9;
+            //
+            //$number=100;
+            //$firstName="Rakib";
+            //$lastName="Hasan";
+            // echo $firstName.' '.$lastName;
+            //variable
+            //$name="Rakib";
+            //ctrl+spacebar
 
 
 
-    //Arithmetic Operator
-    //Binary(+,-,*,/,%)
-    // Unary(++,--,(-))-----
-    //Assignment Operator
-    // =, +=, -=, *=, /=, %=
-    //>, >=, <, <=, !=, ===, !==
-    //Logical Operator
-    //&&, ||, !
-// Arithmetic Operator
-//    Binary(+,-,*,/,%)
-        $x=10;
-        $y=20;
-        echo ($x+$y);
-        echo ('<br/>');
-        echo ($x-$y);
-        echo ('<br/>');
-        echo ($x*$y);
-        echo ('<br/>');
-        echo ($x/$y);
-        echo ('<br/>');
-        echo ($x%$y);
-        echo ('<br/>');
-//Unary
-        $x=10;
-        $y=20;
-        echo ($x++);
-        echo ('<br/>');
-        echo ($x);
-        echo ('<br/>');
-        echo (++$x);
-        echo ('<br/>');
-        echo ($x);
-        echo ('<br/>');
-        echo ($x--);
-        echo ('<br/>');
-        echo ($x);
-        echo ('<br/>');
-        echo (--$x);
-        echo ('<br/>');
-        echo ($x);
-        echo ('<br/>');
-//-----------------------------
-  //Assignment Operator
-
-        $x=40;
-        $y=40;
-        echo ($x+=$y);
-        echo ('<br/>');
-        echo ($x-=$y);
-        echo ('<br/>');
-        echo ($x*=$y);
-        echo ('<br/>');
-        echo ($x/=$y);
-        echo ('<br/>');
-        echo ($x%=$y);
-        echo ('<br/>');
-//-------------
-//Conditional operator
+            /*Arithmetic Operator
+            Binary(+,-,*,/,%)
+             Unary(++,--,(-))-----
+            Assignment Operator
+             =, +=, -=, *=, /=, %=
+            Conditional Operator
+            >, >=, <, <=, !=, ===, !==
+            Logical Operator
+            &&, ||, !
+            */          echo 'Binary operator';
             $x=10;
             $y=20;
+            echo ($x+$y);
+            echo ('<hr/>');
+            echo ($x-$y);
+            echo ('<hr/>');
+            echo ($x*$y);
+            echo ('<hr/>');
+            echo ($x/$y);
+            echo ('<hr/>');
+            echo ($x%$y);
+            echo ('<hr/>');
+            echo 'Unary operator';
+            $x=10;
+            $y=20;
+            echo ($x++);
+            echo ('<hr/>');
+            echo ($x);
+            echo ('<hr/>');
+            echo (++$x);
+            echo ('<hr/>');
+            echo ($x);
+            echo ('<hr/>');
+            echo ($x--);
+            echo ('<hr/>');
+            echo ($x);
+            echo ('<hr/>');
+            echo (--$x);
+            echo ('<hr/>');
+            echo ($x);
+            echo ('<hr/>');
+            //-----------------------------
+            echo 'Assignment Operator';
+
+            $x=40;
+            $y=40;
+            echo ($x+=$y);
+            echo ('<hr/>');
+            echo ($x-=$y);
+            echo ('<hr/>');
+            echo ($x*=$y);
+            echo ('<hr/>');
+            echo ($x/=$y);
+            echo ('<hr/>');
+            echo ($x%=$y);
+            echo ('<hr/>');
+            //-------------
+            echo 'Conditional operator';
+            $x=10;
+            $y=20;
+            echo('<hr/>');
             echo($x>$y);
-            echo('<br/>');
+            echo('<hr/>');
             echo($x>=$y);
-            echo('<br/>');
+            echo('<hr/>');
             echo($x<$y);
-            echo('<br/>');
-            echo($x<=$y);
-            echo('<br/>');
+            echo('<hr/>');
             echo($x==$y);
-            echo('<br/>');
+            echo('<hr/>');
             echo($x!=$y);
-            echo('<br/>');
+            echo('<hr/>');
             $x='20';
-            $x=20;
+            $y=20;
             echo($x===$y);
-            echo('<br/>');
+            echo('<hr/>');
             echo($x!==$y);
-            echo('<br/>');
-//-------------------------------
-echo 'Logical Operator';
-        $x=10;
-        $y=20;
-        $z=30;
-        echo(($x>$y)&&($y>$z));
-        echo('<br/>');
-        echo(($x>$y)&&($y<$z));
-        echo('<br/>');
-        echo(($x<$y)&&($y>$z));
-        echo('<br/>');
-        echo(($x<$y)&&($y<$z));
-        echo('<br/>');
-        echo(($x>$y)||($y>$z));
-        echo('<br/>');
-        echo(($x>$y)||($y<$z));
-        echo('<br/>');
-        echo(($x<$y)||($y>$z));
-        echo('<br/>');
-        echo(($x<$y)||($y<$z));
-        echo('<br/>');
+            echo('<hr/>');
+            echo 'Logical Operator';
+            $x=10;
+            $y=20;
+            $z=30;
+            echo('<hr/>');
+            echo(($x>$y)&&($y>$z));
+            echo('<hr/>');
+            echo(($x>$y)&&($y<$z));
+            echo('<hr/>');
+            echo(($x<$y)&&($y>$z));
+            echo('<hr/>');
+            echo(($x<$y)&&($y<$z));
+            echo('<hr/>');
+            echo(($x>$y)||($y>$z));
+            echo('<hr/>');
+            echo(($x>$y)||($y<$z));
+            echo('<hr/>');
+            echo(($x<$y)||($y>$z));
+            echo('<hr/>');
+            echo(($x<$y)||($y<$z));
+            echo('<hr/>');
 
-        $a=true;
-        echo (!$a);
-        echo('<br/>');
-        $a=false;
-        echo (!$a);
-        echo('<br/>');
-//----------------------------------
-//statement
-/*
- * Single line statement
- * Conditional statement
-     * if
-     * if else
-     *if else if
-     * switch
- * Repeated statement
-     *for
-     * while
-     * do while
-     * foreach
- *
- */
- echo 'Conditional Statement';
+            $a=true;
+            echo (!$a);
+            echo('<hr/>');
+            $a=false;
+            echo (!$a);
+            echo('<hr/>');
+            //----------------------------------
+            ////statement
+            ///*
+            // * Single line statement
+            // * Conditional statement
+            //     * if
+            //     * if else
+            //     *if else if
+            //     * switch
+            // * Repeated statement
+            //     *for
+            //     * while
+            //     * do while
+            //     * /
 
-        $x=10;
-        $y=20;
-        $z =$x+$y;
-        if($x>$y){
+            echo 'Single line statement';
+            echo ('<hr/>');
+            $x=10;
+            $y=20;
+            $z=$x+$y;
             echo $z;
-        }
-echo('<br/>');
-        $x=10;
-        $y=20;
-        if($x>$y){
-        echo ('Hello World');
-        }else{
-        echo ('Hello Javascript');
-        }
+            echo ('<hr/>');
+            echo 'Conditional Statement';
+            echo ('<hr/>');
+            $x=10;
+            $y=20;
+            $z =$x+$y;
+            if($x>$y){
+                echo $z;
+            }
+            echo ('<hr/>');
+            $x=10;
+            $y=20;
+            if($x>$y){
+                echo ('Hello World');
+            }else{
+                echo ('Hello Javascript');
+            }
+            echo ('<hr/>');
 
-echo('<br/>');
             $x=100;
             $y=20;
             $z=40;
             if($x>$y){
                 echo ('Hello World');
             }else if ($y>$z){
-             echo ('Hello Javascript');
+                echo ('Hello Javascript');
             }else if ($z<$x){
                 echo ('Hello BITM');
             }else{
                 echo ('Hello Dhaka');
             }
+            echo ('<hr/>');
 
-
-echo('<br/>');
-        $x=10;
-        switch ($x) {
-            case 5:
-                echo('Hello World');
-                break;
-            case 10:
-                echo('Hello Bangladesh');
-                break;
-            case 15:
-                echo('Hello Dhaka');
-                break;
-            default:
-                echo ('Hello Khulna');
-        }
-echo('<br/>');
-
-           $x;
-              for ($x=10; $x<=15; $x++){
-           echo('Hello World<br/>');
-        }
-         $x=10;
-        while ($x<15){
-            echo('Hello World<br/>');
-            $x++
-        }
 
             $x=10;
-             do {
-    echo('Hello World<br/>');
-           $x++
-        }while ($x<=15)
+            switch ($x) {
+                case 5:
+                    echo('Hello World');
+                    break;
+                case 10:
+                    echo('Hello Bangladesh');
+                    break;
+                case 15:
+                    echo('Hello Dhaka');
+                    break;
+                default:
+                    echo ('Hello Khulna');
+            }
+
+            echo ('<hr/>');
+
+//RepeatetStatement:
+// for
+// while
+// do while
+// for in
+           echo 'Repeatet statement (for)';
+             echo ('<hr/>');
+
+                   $x;
+              for ($x=10; $x<=15; $x++){
+                  echo ('Hello World<br/>');
+              }
+
+             echo ('<hr/>');
+                   $x;
+                for ($x=56; $x<=61; $x++){
+                    echo ($x.'<br/>');
+                }
+             echo ('<hr/>');
+                 $x;
+                for ($x=99; $x>=88; $x--){
+                    echo ($x.'<br/>');
+                }
+
+             echo ('<hr/>');
+
+             echo 'while';
+                 $x=10;
+                while ($x<15){
+                    echo ('Hello World<br/>');
+                    $x++;
+                }
+             echo ('<hr/>');
+             echo 'do while';
+                $x=10;
+            do {
+                echo ('Hello World<br/>');
+                $x++;
+                    }while ($x<=14);
+
+            echo ('<hr/>');
 
 
-
-
-//?>
-<!---->
+?>
+    <!---->
 <?php
 //
 //function demo(){
